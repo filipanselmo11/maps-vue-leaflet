e6bde89eeeda8b32ebe76ed62c43eff62a5fa59f
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-06-23 10:17:51 -0400

    Mapa, com vue, leaflet e bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <map-component></map-component>
+    </div>
+    <!-- <div class="col-md-3">
+      <layers-checkbox></layers-checkbox>
+    </div> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import LayersCheckbox from './components/LayersCheckbox.vue'
+import MapComponent from './components/MapComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MapComponent,
+    // LayersCheckbox
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
